090408aaeb3c338d5d43e593f9f32250abb68ee9
marcensie <aquila19908@gmail.com>
2022-10-06 12:50:44 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -27,18 +26,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _heightController = TextEditingController();
   final _weightController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
 
   double? index;
-
   String status = '';
-
+  String title = 'Lab 2';
 
   void _calculate() {
    double height = double.parse(_heightController.text);
    double weight = double.parse(_weightController.text);
-
-
     setState(() {
       index = weight / ((height * height)/10000);
       if (index! < 18.5) {
@@ -57,32 +52,37 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lab 2',),
+        title: Text(title,),
         centerTitle: true,
         backgroundColor: Colors.green,
         elevation: 0.0,
 
       ),
         backgroundColor: Colors.white,
-        body: Center(
+      body: Center(
           child: SizedBox(
             width: 320,
               child: Padding(
-                padding: const EdgeInsets.all(20),
+                padding: const EdgeInsets.all(30),
                 child: Column(
                   //mainAxisSize: MainAxisSize.min,
                   children: [
                     TextField(
                       keyboardType:
                       const TextInputType.numberWithOptions(decimal: true),
-                      decoration:
-                      const InputDecoration(labelText: 'Height (cm)'),
+                      decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                        labelText: "Height",
+                      ),
                       controller: _heightController,
                     ),
                     TextField(
                       keyboardType:
                       const TextInputType.numberWithOptions(decimal: true),
-                      decoration: const InputDecoration(labelText: 'Weight (kg)',),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Weight",
+                      ),
                       controller: _weightController,
                     ),
                     const SizedBox(
